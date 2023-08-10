bc188d22824dcae6ff0cb3a376308be61f9032a5
Сергій Сябро <serhiisyabro@Sergijs-MBP.lan>
2023-08-10 22:39:35 +0300

    first homework

---
+++ HomeWork/1/js/script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const firstNumber = 100;
+const secondNumber = 5;
+
+const addition = firstNumber + secondNumber;
+const subtraction = firstNumber - secondNumber;
+const multiplication = firstNumber * secondNumber;
+const division = firstNumber / secondNumber;
+const exponentiation = firstNumber ** secondNumber;
+
+console.log('addition:', addition);
+console.log('subtraction:', subtraction);
+console.log('multiplication:', multiplication);
+console.log('division:', division);
+console.log('exponentiation', exponentiation);
