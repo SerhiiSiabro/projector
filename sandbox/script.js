21c77e416d2aa8930ddab0f646431bff1301c6aa
Serhii Siabro <serhii.syabro@icloud.com>
2023-09-13 23:25:07 +0300

    test to create normal branch

--- sandbox/script.js
+++ sandbox/script.js
@@ -45,3 +45,35 @@ const priceData = {
 
 durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'seconds'); // поверне '86400 seconds'
 durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days'); // поверне '362 days'
+
+
+function iterativeOddSumTo(number) {
+    let result = 0;
+    for (let i = 1; i <= number; i++) {
+      if (i % 2 !== 0) {
+        result += i;
+      } 
+    }
+    return result;
+  };
+    
+  console.log(iterativeOddSumTo(1)) // 1
+  console.log(iterativeOddSumTo(9)) // 25
+  console.log(iterativeOddSumTo(10)) // 25
+  
+  function recursiveOddSumTo(number, i = 1) {
+    if (number === 1) {
+      return 1;
+   }
+    if (number < i) {
+      return 0;
+    }
+    if (number % 2 !== 0) {
+      return number + recursiveOddSumTo(number -2, i = 1)
+    }
+    return recursiveOddSumTo(number - 1)
+  };
+    
+  console.log(recursiveOddSumTo(1)) // 1
+  console.log(recursiveOddSumTo(9)) // 25
+  console.log(recursiveOddSumTo(10)) // 25
