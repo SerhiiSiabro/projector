7b2a5658cd7dc3b1fac94ab5f8f62765b5d8250c
Serhii Siabro <serhii.syabro@icloud.com>
2023-09-13 23:30:35 +0300

    added recomendations to recursion HW

---
+++ HomeWork/5 HW2  Objects and basics of working with functions/js/script.js
@@ -0,0 +1,30 @@
+function iterativeOddSumTo(number) {
+  let result = 0;
+  for (let i = 1; i <= number; i++) {
+    if (i % 2 !== 0) {
+      result += i;
+    } 
+  }
+  return result;
+};
+  
+console.log(iterativeOddSumTo(1)) // 1
+console.log(iterativeOddSumTo(9)) // 25
+console.log(iterativeOddSumTo(10)) // 25
+
+function recursiveOddSumTo(number, i = 1) {
+  if (number === 1) {
+    return 1;
+ }
+  if (number < i) {
+    return 0;
+  }
+  if (number % 2 !== 0) {
+    return number + recursiveOddSumTo(number -2, i = 1)
+  }
+  return recursiveOddSumTo(number - 1)
+};
+  
+console.log(recursiveOddSumTo(1)) // 1
+console.log(recursiveOddSumTo(9)) // 25
+console.log(recursiveOddSumTo(10)) // 25
