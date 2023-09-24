73f7314cb29b862a8cfdcc036fe7e7d72d196b96
Serhii Siabro <serhii.syabro@icloud.com>
2023-09-24 13:39:54 +0300

    install Prettier; deleted variable 'result'.

--- HomeWork/7_Closure,_residual_parameters,_sets/js/script.js
+++ HomeWork/7_Closure,_residual_parameters,_sets/js/script.js
@@ -1,19 +1,19 @@
-console.log(addThemAll(2,4)); // 6
-console.log(addThemAll(1,2,3,4)); // 10
-console.log(addThemAll(5,5,10)); // 20
+console.log(addThemAll(2, 4)); // 6
+console.log(addThemAll(1, 2, 3, 4)); // 10
+console.log(addThemAll(5, 5, 10)); // 20
 
 function addThemAll(...args) {
-    let result = 0;
-    args.reduce((result, currentValue) => result + currentValue);
-    return result;
+  return args.reduce(
+    (previousValue, currentValue) => previousValue + currentValue
+  );
 }
 
-console.log(multiply(5)(5))		// 25
-console.log(multiply(2)(-2))	        // -4
-console.log(multiply(4)(3))		// 12
+console.log(multiply(5)(5)); // 25
+console.log(multiply(2)(-2)); // -4
+console.log(multiply(4)(3)); // 12
 
 function multiply(a) {
-	return function(b) {
-        return a * b;
-    }
+  return function (b) {
+    return a * b;
+  };
 }
