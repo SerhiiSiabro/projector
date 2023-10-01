d2bf01f00d2202da3ce0824ae6bbfe9e3ae69eee
Serhii Siabro <sergey.syabro95@gmail.com>
2023-10-01 19:07:41 +0300

    started HW

---
+++ HomeWork/task_on_selectors/js/script.js
@@ -0,0 +1,8 @@
+let spanWithClass = document.querySelector(".hatredLevelCounter");
+console.log(spanWithClass);
+
+let headerTwo = document.getElementById("headerTwo");
+console.log(headerTwo);
+
+let firstgChildFirstSection = document.getElementsByClassName("firstSection");
+console.log(firstgChildFirstSection[0].childNodes.);
