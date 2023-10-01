b252f7d254a968abbe3cd92eba0ab00511131241
Serhii Siabro <sergey.syabro95@gmail.com>
2023-10-01 10:45:15 +0300

    added therd task

--- HomeWork/HW4 — Execution planning, object methods, execution context, call and apply methods/js/script.js
+++ HomeWork/HW4 — Execution planning, object methods, execution context, call and apply methods/js/script.js
@@ -24,20 +24,76 @@ const movies = [
     runningTimeInMinutes: 107,
   },
 ];
-
-console.log(movies.sort(byProperty("releaseYear", ">")));
-// // виведе масив фільмів посортованих по року випуску, від старішого до новішого
-console.log(movies.sort(byProperty("runningTimeInMinutes", "<")));
+// debugger;
+// console.log(movies.sort(byProperty("releaseYear", ">")));
+// виведе масив фільмів посортованих по року випуску, від старішого до новішого
+// console.log(movies.sort(byProperty("runningTimeInMinutes", "<")));
 // // виведе масив фільмів посортованих по їх тривалості, від найдовшого до найкоротшого
 console.log(movies.sort(byProperty("movieName", ">")));
 // // виведе масив фільмів посортованих по назві, в алфавітному порядку
 
 function byProperty(property, direction) {
-  return (a, b) => {
-    if(direction === ">") {
-      return a[property] > b[property] ? 1 : -1;
-    } else if(direction === "<") {
-      return a[property] < b[property] ? 1 : -1;
+  if (property === "releaseYear" || property === "runningTimeInMinutes") {
+    switch (direction) {
+      case ">":
+        return (a, b) => a[property] - b[property];
+      case "<":
+        return (a, b) => b[property] - a[property];
+      default:
+        break;
     }
   }
+  if (property === "movieName") {
+    // byString(property, direction);
+    return (a, b) => {
+      const firstValue = a[property].toUpperCase();
+      const secondValue = b[property].toUpperCase();
+      switch (direction) {
+        case ">":
+          if (firstValue < secondValue) {
+            return -1;
+          }
+          if (firstValue > secondValue) {
+            return 1;
+          }
+          return 0;
+        case "<":
+          if (firstValue > secondValue) {
+            return -1;
+          }
+          if (firstValue < secondValue) {
+            return 1;
+          }
+          return 0;
+        default:
+          break;
+      }
+    };
+  }
 }
+// function byString(property, direction) {
+//   return (a, b) => {
+//     const firstValue = a[property].toUpperCase();
+//     const secondValue = b[property].toUpperCase();
+//     switch (direction) {
+//       case ">":
+//         if (firstValue < secondValue) {
+//           return -1;
+//         }
+//         if (firstValue > secondValue) {
+//           return 1;
+//         }
+//         return 0;
+//       case "<":
+//         if (firstValue > secondValue) {
+//           return -1;
+//         }
+//         if (firstValue < secondValue) {
+//           return 1;
+//         }
+//         return 0;
+//       default:
+//         break;
+//     }
+//   };
+// }
