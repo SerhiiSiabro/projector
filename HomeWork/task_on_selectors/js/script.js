ff1235dd29bbeed18c5b01493aec8bceabff66dc
Serhii Siabro <sergey.syabro95@gmail.com>
2023-10-01 23:13:11 +0300

    added HW

--- HomeWork/task_on_selectors/js/script.js
+++ HomeWork/task_on_selectors/js/script.js
@@ -1,8 +1,13 @@
-let spanWithClass = document.querySelector(".hatredLevelCounter");
-console.log(spanWithClass);
-
 let headerTwo = document.getElementById("headerTwo");
 console.log(headerTwo);
 
-let firstgChildFirstSection = document.getElementsByClassName("firstSection");
-console.log(firstgChildFirstSection[0].childNodes.);
+let elements = document.getElementsByClassName("firstSection");
+let firstgElementFirstSection = elements[0].firstElementChild;
+console.log(firstgElementFirstSection);
+
+let list = document.querySelector("ul");
+let elementWithOptionFive = list.lastElementChild.previousElementSibling;
+console.log(elementWithOptionFive);
+
+let hatredLevelBlock = document.querySelectorAll(".hatredLevelBlock");
+console.log(hatredLevelBlock);
