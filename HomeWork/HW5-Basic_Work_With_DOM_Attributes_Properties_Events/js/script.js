f3df6279500b6dec01a8f59e7fc9b51d3bb5545b
Serhii Siabro <sergey.syabro95@gmail.com>
2023-10-07 22:30:21 +0300

    added workable option

--- HomeWork/HW5-Basic_Work_With_DOM_Attributes_Properties_Events/js/script.js
+++ HomeWork/HW5-Basic_Work_With_DOM_Attributes_Properties_Events/js/script.js
@@ -2,6 +2,38 @@
 
 const button = document.querySelector(".button");
 const wrapper = document.querySelector(".wrapper");
+const infoClick = document.getElementById("infoClick");
+
+button.addEventListener("click", function () {
+  const timeNow = new Date();
+  infoClick.style.display = "block";
+  if (button.textContent === "Turn off") {
+    turnOn();
+    wrapper.classList.toggle("wrapper-black");
+
+    infoClick.textContent = `Last turn off: ${formatDate(timeNow)}`;
+
+    setInfo();
+  } else if (button.textContent === "Turn on") {
+    turnOff();
+    wrapper.classList.toggle("wrapper-black");
+
+    infoClick.textContent = `Last turn on: ${formatDate(timeNow)}`;
+
+    setInfo();
+  }
+});
+
+if (localStorage.getItem("collorWrapper")) {
+  const storedTime = JSON.parse(localStorage.getItem("time"));
+  const storedButtonText = JSON.parse(localStorage.getItem("buttonText"));
+  const storedCollorWrapper = JSON.parse(localStorage.getItem("collorWrapper"));
+
+  button.textContent = storedButtonText;
+  infoClick.style.display = "block";
+  infoClick.textContent = storedTime;
+  wrapper.classList.toggle("wrapper-black", storedCollorWrapper);
+}
 
 function turnOn() {
   return (button.textContent = "Turn on");
@@ -11,12 +43,31 @@ function turnOff() {
   return (button.textContent = "Turn off");
 }
 
-button.addEventListener("click", function () {
-  if (button.textContent === "Turn off") {
-    turnOn();
-    wrapper.classList.toggle("wrapper-black");
-  } else if (button.textContent === "Turn on") {
-    turnOff();
-    wrapper.classList.toggle("wrapper-black");
-  }
-});
+function padTo2Digits(num) {
+  return num.toString().padStart(2, "0");
+}
+
+function formatDate(date) {
+  return (
+    [
+      padTo2Digits(date.getDate()),
+      padTo2Digits(date.getMonth() + 1),
+      date.getFullYear(),
+    ].join("-") +
+    " " +
+    [
+      padTo2Digits(date.getHours()),
+      padTo2Digits(date.getMinutes()),
+      padTo2Digits(date.getSeconds()),
+    ].join(":")
+  );
+}
+
+function setInfo() {
+  localStorage.setItem("time", JSON.stringify(infoClick.textContent));
+  localStorage.setItem("buttonText", JSON.stringify(button.textContent));
+  localStorage.setItem(
+    "collorWrapper",
+    JSON.stringify(wrapper.classList.contains("wrapper-black"))
+  );
+}
