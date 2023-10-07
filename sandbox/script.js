80b3d90e66614f43635a804e17264ed6773f04b1
Serhii Siabro <sergey.syabro95@gmail.com>
2023-10-07 18:42:52 +0300

    added sandbox

--- sandbox/script.js
+++ sandbox/script.js
@@ -1,12 +1,1 @@
-"use strict";
-
-const buttonLight = document.getElementById("buttonLight");
-const buttonDark = document.getElementById("buttonDark");
-
-buttonLight.addEventListener("click", () => {
-  document.body.style.backgroundColor = "white";
-});
-
-buttonDark.addEventListener("click", () => {
-  document.body.style.backgroundColor = "darkblue";
-});
+const input = document;
